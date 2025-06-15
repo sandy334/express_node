74d5fef36e7392b713c0335cdc80d2ccdb92469e
sandy334 <hrsandesh3@gmail.com>
2025-06-15 19:08:37 +0530

    Implement contact API route using separate router module

--- express-project/my-contacts-backend/server.js
+++ express-project/my-contacts-backend/server.js
@@ -3,9 +3,7 @@ const dotenv = require('dotenv').config();
 const app=express();
 const port = process.env.PORT||5000;
 
-app.get('/api/contacts', (req, res) => {
-    res.status(200).json({message:'Get all contacts '});
-});
+app.use("/api/contacts", require("./routes/contactRoutes"));
  
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
