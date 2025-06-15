ad1eb2ea5e7b454952fd37a515bcea091f25822c
sandy334 <hrsandesh3@gmail.com>
2025-06-15 18:58:57 +0530

    Add dotenv dependency and configure environment variables; set up contact API route

--- express-project/my-contacts-backend/server.js
+++ express-project/my-contacts-backend/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
 const app=express();
-const port = 5000;
+const port = process.env.PORT||5000;
+
+app.get('/api/contacts', (req, res) => {
+    res.status(200).json({message:'Get all contacts '});
+});
  
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
