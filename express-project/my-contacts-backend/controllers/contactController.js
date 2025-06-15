4e895ad0f04600370c57a865de9451f9c8157c68
sandy334 <hrsandesh3@gmail.com>
2025-06-15 23:16:19 +0530

    Add async support to contact controller methods and enhance error handling

--- express-project/my-contacts-backend/controllers/contactController.js
+++ express-project/my-contacts-backend/controllers/contactController.js
@@ -1,7 +1,8 @@
+
 // @desc Get all contacts
 // @route GET /api/contacts
 // @access public
-const getContacts = (req, res) => {
+const getContacts = async(req, res) => {
     res.status(200).json({ message: "Get all contacts" });
 };
 
@@ -9,7 +10,7 @@ const getContacts = (req, res) => {
 // @desc Create new contact
 // @route POST /api/contacts
 // @access public
-const createContact = (req, res) => {
+const createContact = async(req, res) => {
     console.log("The request body is:", req.body);
     const { name, email, phone } = req.body;
     if (!name || !email || !phone) {
@@ -22,21 +23,21 @@ const createContact = (req, res) => {
 // @desc Get single contact
 // @route GET /api/contacts/:id
 // @access public
-const getContact = (req, res) => {
+const getContact = async(req, res) => {
     res.status(200).json({ message: `Get contact for ${req.params.id}` });
 };
 
 // @desc Update contact
 // @route PUT /api/contacts/:id
 // @access public
-const updateContact = (req, res) => {
+const updateContact = async(req, res) => {
     res.status(200).json({ message: `Update contact for ${req.params.id}` });
 };
 
 // @desc Delete contact
 // @route DELETE /api/contacts/:id
 // @access public
-const deleteContact = (req, res) => {
+const deleteContact = async(req, res) => {
     res.status(200).json({ message: `Delete contact for ${req.params.id}` });
 };
 
--- express-project/my-contacts-backend/middleware/errorHandler.js
+++ express-project/my-contacts-backend/middleware/errorHandler.js
@@ -1,20 +1,41 @@
+const {constants} = require("../constants");
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500; // Set default status code to 500 if not set
     switch (statusCode) {
-        case 400:
+        case constants.VALIDATION_ERROR:
             res.json({
                 title: "Validation Failed",
                 message: err.message,
                 stack: err.stack,
             });
             break;
-        case 404:
+        case constants.NOT_FOUND:
             res.json({
                 title: "Not Found",
                 message: err.message,
                 stack: err.stack,
             });
+        case constants.UNAUTHORIZED:
+            res.json({
+                title: "Unauthorized",
+                message: err.message,
+                stack: err.stack,
+            });
+        case constants.FORBIDDEN:
+            res.json({
+                title: "Forbidden",
+                message: err.message,
+                stack: err.stack,
+            });
+        case constants.SERVER_ERROR:
+            res.json({
+                title: "Server Error",
+                message: err.message,
+                stack: err.stack,
+            });
             default:
+            console.log("No error, all good!");
+            
             break;
        
     }
