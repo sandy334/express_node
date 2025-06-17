a87a2ce413a11f9f7750628647bdde75d1055aa4
sandy334 <hrsandesh3@gmail.com>
2025-06-17 18:39:35 +0530

    asyncHandler

--- express-project/my-contacts-backend/controllers/contactController.js
+++ express-project/my-contacts-backend/controllers/contactController.js
@@ -1,10 +1,10 @@
-
+const asyncHandler = require("express-async-handler");
 // @desc Get all contacts
 // @route GET /api/contacts
 // @access public
-const getContacts = async(req, res) => {
+const getContacts = asyncHandler(async(req, res) => {
     res.status(200).json({ message: "Get all contacts" });
-};
+});
 
 
 // @desc Create new contact
