7e5036e47490bed5ff277b7a3819d42ebe2152c4
sandy334 <hrsandesh3@gmail.com>
2025-06-17 18:45:44 +0530

    comit

--- express-project/my-contacts-backend/routes/contactRoutes.js
+++ express-project/my-contacts-backend/routes/contactRoutes.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { 
-    getContacts, 
-    createContact, 
-    getContact,
-    updateContact, 
-    deleteContact 
-} = require('../controllers/contactController');
+const { getContacts, createContact, updateContact, deleteContact } = require('../controllers/contactController');
+// Import the controller functions from contactController.js
+router.route("/").get(getContacts ).post(createContact )
 
-router.route("/").get(getContacts).post(createContact);
+router.route("/:id").get( getContact).put( updateContact ).delete( deleteContact )
 
-router.route("/:id")
-    .get(getContact)
-    .put(updateContact)
-    .delete(deleteContact);
+
 
 module.exports = router;
--- express-project/my-contacts-backend/server.js
+++ express-project/my-contacts-backend/server.js
@@ -1,19 +1,10 @@
 const express = require('express');
-const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config();
-const app = express();
+const app=express();
+const port = process.env.PORT||5000;
 
-// Middleware for parsing JSON bodies
-app.use(express.json());
-
-const port = process.env.PORT || 5000;
-
-// Routes
 app.use("/api/contacts", require("./routes/contactRoutes"));
-
-// Error handling middleware
-app.use(errorHandler);
-
+ 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
