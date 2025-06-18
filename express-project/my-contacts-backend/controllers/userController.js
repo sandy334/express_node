de285d9721b3030a2db973db66f486d0cdbcda1b
sandy334 <hrsandesh3@gmail.com>
2025-06-19 02:26:50 +0530

    check email and pass

--- express-project/my-contacts-backend/controllers/userController.js
+++ express-project/my-contacts-backend/controllers/userController.js
@@ -49,6 +49,10 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error('Please add all fields');
+    }
     res.json({ message: 'Login User' });
 });
 
