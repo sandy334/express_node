62ba355d1109031e6680be9573c3d48daad7da15
sandy334 <hrsandesh3@gmail.com>
2025-06-19 00:45:32 +0530

    solver connection bug

--- express-project/my-contacts-backend/models/contactModel.js
+++ express-project/my-contacts-backend/models/contactModel.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose');
 
 const contactSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please add a name'],
-    },
-    email: {
-        type: String,
-        required: [true, 'Please add an email'],
-        unique: true,
-    },
-    phone: {
-        type: String,
-        required: [true, 'Please add a phone number'],
-    }
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: [true, 'Please add a phone number'],
+  }
 }, {
-    timestamps: true,
+  timestamps: true,
 });
 
-module.exports = mongoose.model('Contact', contactSchema);
+module.exports = mongoose.model("Contact", contactSchema); // ✅ no plural/s
