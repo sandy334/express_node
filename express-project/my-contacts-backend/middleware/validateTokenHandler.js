669c5c2a24ca162661414e974ad4bc0e7ff2646e
sandy334 <hrsandesh3@gmail.com>
2025-06-19 03:03:23 +0530

    use

--- express-project/my-contacts-backend/middleware/validateTokenHandler.js
+++ express-project/my-contacts-backend/middleware/validateTokenHandler.js
@@ -1,4 +1,17 @@
 const asyncHandler = require('express-async-handler');
 const jwt = require('jsonwebtoken');
 
-const validateToken= asyncHandler(async (req, res, next) )
+const validateToken= asyncHandler(async (req, res, next) )=> {
+    let token;
+
+    // Check if token is in the headers
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            // Get token from header
+            token = req.headers.authorization.split(' ')[1];
+            // Verify token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            // Attach user to the request object
+            req.user = decoded.user;
+            next(); 
+        }
