36508a1159684e8d47522ea6345e9f34b21c429d
sandy334 <hrsandesh3@gmail.com>
2025-06-19 01:56:31 +0530

    commited

--- express-project/my-contacts-backend/controllers/userController.js
+++ express-project/my-contacts-backend/controllers/userController.js
@@ -1,8 +1,27 @@
 const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
 //desc Register a user
 // @route POST /api/users
 // @access Public   
-const registerUser = asyncHandler(async (req, res) => {res.json({ message: 'Register User' })});
+const registerUser = asyncHandler(async (req, res) => {
+    const {username, email, password} = req.body;
+    if (!username || !email || !password) {
+        res.status(400);
+        throw 
+        new Error('Please add all fields');
+    }
+    const userAvailable = await User.findOne({ email });
+    if (userAvailable) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    // Hash password
+    const user = await User.create({
+        username,
+        email,
+        password
+    });
 
 //desc Login a user
 // @route POST /api/users/login
