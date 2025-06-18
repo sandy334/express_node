7433d034254ce893f8ef54934e1d4ad99bf016c5
sandy334 <hrsandesh3@gmail.com>
2025-06-18 22:56:38 +0530

    done

--- express-project/my-contacts-backend/server.js
+++ express-project/my-contacts-backend/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const connectDB = require('./config/dbConnection');
-const dotenv = require('dotenv').config();
-connectDB();
-const app=express();
-const port = process.env.PORT||5000;
+const dotenv = require('dotenv').config(); // Loads .env file
 
+connectDB(); // Connect to MongoDB
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Routes
 app.use("/api/contacts", require("./routes/contactRoutes"));
- 
+
+// Start server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
---
+++ models/contactmodel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: true,
+    },
+    phone: {
+        type: String,
+        required: [true, 'Please add a phone number'],
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+    },
+}, {
+    timestamps: true,
+});
